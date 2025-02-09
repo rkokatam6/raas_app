866ae904d3e0ccb738fe62a1f8cb4c9a924521c5
Kokatam, Rishika <165109213+rkokatam6@users.noreply.github.com>
2025-02-09 14:07:17 -0500

    pages

---
+++ src/Formations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Formations = () => {
+  return (
+    <div>
+      <h1>Formations Page</h1>
+      {/* Add your formations content here */}
+    </div>
+  );
+};
+
+export default Formations;
